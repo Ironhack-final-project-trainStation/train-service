74800951bf2c4b4821cc00d3bcb0af31731d89a5
Irenevn16 <irenevn16@gmail.com>
2025-05-26 17:43:52 +0200

    CRUD

--- src/main/java/train_service/train_service/controllers/TrainController.java
+++ src/main/java/train_service/train_service/controllers/TrainController.java
@@ -3,10 +3,7 @@ package train_service.train_service.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import train_service.train_service.exceptions.TrainNotFoundException;
 import train_service.train_service.models.Train;
 import train_service.train_service.service.TrainService;
@@ -25,6 +22,31 @@ public class TrainController {
             return new ResponseEntity<>(foundTrain, HttpStatus.OK);
         } catch (TrainNotFoundException exception) {
             return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+    @PostMapping
+    public ResponseEntity<Train> createTrain (@RequestBody Train train) {
+        Train newTrain = trainService.saveTrain(train);
+        return new ResponseEntity<>(newTrain, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateTrain(@PathVariable String id, @RequestBody Train train) {
+        try {
+            Train updatedTrain = trainService.updateTrain(id, train);
+            return new ResponseEntity<>(updatedTrain, HttpStatus.OK);
+        } catch (TrainNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteTrain (@PathVariable String id) {
+        try {
+            trainService.deleteTrain(id);
+            return new ResponseEntity<>("Train deleted succesfully", HttpStatus.OK);
+        } catch (TrainNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 }
